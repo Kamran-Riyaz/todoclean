8a27274daaf3ce3bebbaeb16fbea0468db6e2b72
Kamran-Riyaz <kamranriyazw@gmail.com>
2026-05-06 20:53:27 +0530

    implement renderTodos function to display todo items and update localStorage

--- app.js
+++ app.js
@@ -4,3 +4,24 @@ const list = document.getElementById("todo-list");
 
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
+function renderTodos() {
+  list.innerHTML = "";
+
+  todos.forEach((todo, index) => {
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+            <span class="${todo.completed ? "completed" : ""}">
+            ${todo.text}
+            </span>
+            <div>
+                <button onclick="toogleTodo(${index})">✔</button>
+                <button onclick="deleteTodo(${index})">✖</button>
+            </div>
+        `;
+
+    list.appendChild(li);
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
